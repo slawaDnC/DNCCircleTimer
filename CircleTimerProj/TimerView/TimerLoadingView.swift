8e3998e063a015d5b462c95477a8bc48e3f0906c
Vyacheslav Razumeenko <vyacheslav.razumeenko@gmail.com>
2022-09-27 16:02:12 +0300

    feature: configure timer view layout

--- CircleTimerProj/TimerView/TimerLoadingView.swift
+++ CircleTimerProj/TimerView/TimerLoadingView.swift
@@ -12,6 +12,47 @@ private typealias View = TimerView.LoadingView
 
 extension TimerView {
     class LoadingView: UIView {
+        private enum Constants {
+            /// needs to turn progress layer in counterclock-wise
+            static let progressLayerRotate = CATransform3DMakeRotation(
+                CGFloat(90 * Double.pi / 180),
+                .zero,
+                .zero,
+                -1)
+        }
+
+        // MARK: - Configuration
+
+        struct Configuration {
+            let lineWidth: CGFloat
+            let backgroundColor: UIColor
+            let progressColor: UIColor
+            let lineCap: CAShapeLayerLineCap = .round
+        }
+
+        // MARK: - Public Properties
+
+        var progress: CGFloat = .zero {
+            didSet {
+                setNeedsDisplay()
+            }
+        }
+
+        // MARK: - Private Properties
+
+        private let progressLayer: CAShapeLayer = .init()
+        private let backgroundLayer: CAShapeLayer = .init()
+
+        private var configuration: Configuration?
+
+        // MARK: - Inits
+
+        convenience init(configuration: Configuration) {
+            self.init(frame: .zero)
+
+            apply(configuration)
+        }
+
         override init(frame: CGRect) {
             super.init(frame: frame)
 
@@ -23,6 +64,33 @@ extension TimerView {
 
             setup()
         }
+
+        override func draw(_ rect: CGRect) {
+            let lineWidth = configuration?.lineWidth ?? .zero
+            let rect: CGRect = rect.insetBy(dx: lineWidth, dy: lineWidth)
+            let circlePath: UIBezierPath = .init(ovalIn: rect)
+            backgroundLayer.path = circlePath.cgPath
+
+            progressLayer.path = circlePath.cgPath
+            progressLayer.strokeStart = .zero
+            progressLayer.strokeEnd = progress
+        }
+
+        func apply(_ configuration: Configuration) {
+            self.configuration = configuration
+
+            backgroundColor = configuration.backgroundColor
+
+            let lineWidth = configuration.lineWidth + configuration.lineWidth
+            backgroundLayer.lineWidth = lineWidth
+            backgroundLayer.strokeColor = configuration.backgroundColor.cgColor
+
+            progressLayer.lineWidth = lineWidth
+            progressLayer.strokeColor = configuration.progressColor.cgColor
+            progressLayer.lineCap = configuration.lineCap
+
+            setNeedsDisplay()
+        }
     }
 }
 
@@ -30,13 +98,15 @@ extension TimerView {
 
 private extension View {
     func setup() {
-        configureSubviews()
-        configureConstraints()
+        configureSublayers()
     }
 
-    func configureSubviews() {
-    }
+    func configureSublayers() {
+        backgroundLayer.fillColor = UIColor.clear.cgColor
+        layer.mask = backgroundLayer
 
-    func configureConstraints() {
+        progressLayer.fillColor = nil
+        layer.addSublayer(progressLayer)
+        layer.transform = Constants.progressLayerRotate
     }
 }
--- CircleTimerProj/TimerView/TimerView.swift
+++ CircleTimerProj/TimerView/TimerView.swift
@@ -11,13 +11,50 @@ import SnapKit
 private typealias View = TimerView
 
 class TimerView: UIView {
-    private lazy var loadingView: LoadingView = .init()
+    // MARK: - Configuration
+    
+    struct Configuration {
+        let font: UIFont
+        let textColor: UIColor
+        let loadingConfiguration: LoadingView.Configuration
 
+        static let `default`: Configuration = .init(
+            font: .systemFont(ofSize: 25, weight: .bold),
+            textColor: .white,
+            loadingConfiguration: .init(lineWidth: 5, backgroundColor: .gray, progressColor: .white))
+    }
+
+    // MARK: - Private UI
+
+    private lazy var loadingView: LoadingView = .init()
     private lazy var countLabel: UILabel = build {
         $0.textAlignment = .center
         $0.font = .systemFont(ofSize: 20, weight: .medium)
         $0.numberOfLines = 1
         $0.text = "10"
+    }
+
+    // MARK: - Public Properties
+
+    var progress: CGFloat {
+        get {
+            loadingView.progress
+        }
+        set {
+            loadingView.progress = newValue
+        }
+    }
+
+    // MARK: - Private Properties
+
+    private var configuration: Configuration?
+
+    // MARK: - Inits
+
+    convenience init(configuration: Configuration) {
+        self.init(frame: .zero)
+
+        apply(configuration)
     }
 
     override init(frame: CGRect) {
@@ -31,6 +68,16 @@ class TimerView: UIView {
 
         setup()
     }
+
+    func apply(_ configuration: Configuration) {
+        self.configuration = configuration
+
+        countLabel.font = configuration.font
+        countLabel.textColor = configuration.textColor
+        loadingView.apply(configuration.loadingConfiguration)
+
+        setNeedsDisplay()
+    }
 }
 
 // MARK: - Private Methods
@@ -39,9 +86,6 @@ private extension View {
     func setup() {
         configureSubviews()
         configureConstraints()
-
-        layer.borderColor = UIColor.black.cgColor
-        layer.borderWidth = 1.0
     }
 
     func configureSubviews() {
